72a5d2494e5e14b40b53afa1038309f55a659db9
Rachatat Bunpat <14316641-rbunpat@users.noreply.gitlab.com>
2024-07-06 23:29:16 +0700

    chore: Add Discord and GitHub as authentication providers

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,10 @@
 
 import { SvelteKitAuth } from "@auth/sveltekit"
 import Google from "@auth/sveltekit/providers/google"
+import Discord from "@auth/sveltekit/providers/discord"
+import GitHub from "@auth/sveltekit/providers/github"
  
 export const { handle, signIn } = SvelteKitAuth({
   trustHost: true,
-  providers: [Google],
+  providers: [Google, Discord, GitHub],
 }) 
